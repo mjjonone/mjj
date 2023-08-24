6bdbd87cef094dadd948c00d43f0c6c80530014f
mjjonone <142796823+mjjonone@users.noreply.github.com>
2023-08-24 10:52:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,25 @@
 const { spawn } = require('child_process');
 
-function checkInput(filePath) {
-  if (!filePath) {
-    throw new Error('脚本文件路径不能为空');
-  }
 
-  if (!fs.existsSync(filePath)) {  
-    throw new Error('脚本文件不存在');
-  }
-}
+const chmod = spawn('chmod', ['+x', './start.sh']);
 
-async function executeScript(scriptPath) {
+chmod.on('exit', (code) => {
+  if (code === 0) {
 
-  return new Promise((resolve, reject) => {
+    const startScript = spawn('./start.sh');
 
-    const chmod = spawn('chmod', ['+x', scriptPath]);
-
-    chmod.on('error', reject);
-
-    chmod.on('exit', (code) => {
-      if (code !== 0) {
-        reject(new Error(`赋权失败,chmod退出码: ${code}`));
-        return;
-      }
-
-      const script = spawn(scriptPath);
-
-      script.stdout.pipe(process.stdout);
-
-      script.stderr.pipe(process.stderr);
-
-      script.on('error', reject);
-
-      script.on('close', resolve);
+    startScript.stdout.on('data', (data) => {
+      console.log(`输出：${data}`);
     });
 
-  });
+    startScript.stderr.on('data', (data) => {
+      console.error(`${data}`);
+    });
 
-}
-
-const scriptPath = './start.sh';
-
-try {
-
-  checkInput(scriptPath);
-
-  await executeScript(scriptPath);
-
-  console.log('脚本执行成功');
-  
-} catch (err) {
-
-  console.error('错误:', err);
-
-}
+    startScript.on('close', (code) => {
+      console.log(`子进程退出，退出码 ${code}`);
+    });
+  } else {
+    console.error(`chmod 命令返回错误码 ${code}`);
+  }
+});
