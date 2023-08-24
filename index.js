a051083be9d6f177d241f152169e335dd19b0493
mjjonone <142796823+mjjonone@users.noreply.github.com>
2023-08-24 11:21:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { spawn } = require('child_process');
 const port = process.env.SERVER_PORT || 3000;
+const express = require("express");
+const app = express();
 
 const chmod = spawn('chmod', ['+x', './start.sh']);
 
@@ -24,4 +26,7 @@ chmod.on('exit', (code) => {
   }
 });
 
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
