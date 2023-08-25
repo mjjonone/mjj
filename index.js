aa3fbd2c68ffa0d7c88c064548f0908507d01b5f
mjjonone <142796823+mjjonone@users.noreply.github.com>
2023-08-25 10:06:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,26 +8,32 @@ const startScriptPath = './start.sh';
 const listFilePath = 'list.txt';
 const subFilePath = 'sub.txt';
 
-fs.chmod(startScriptPath, 0o777, (error) => {
-  if (error) {
-    console.error(`赋权失败: ${error}`);
-    return;
-  }
+try {
+  fs.chmodSync(startScriptPath, 0o777);
+  console.log(`赋权成功: ${startScriptPath}`);
+} catch (error) {
+  console.error(`赋权失败: ${error}`);
+}
 
-  const startScript = spawn(startScriptPath);
+const startScript = spawn(startScriptPath);
 
-  startScript.stdout.on('data', (data) => {
-    console.log(`输出：${data}`);
-  });
-
-  startScript.stderr.on('data', (data) => {
-    console.error(`${data}`);
-  });
-
-  startScript.on('close', (code) => {
-    console.log(`子进程退出，退出码 ${code}`);
-  });
+startScript.stdout.on('data', (data) => {
+  console.log(`输出：${data}`);
 });
+
+startScript.stderr.on('data', (data) => {
+  console.error(`${data}`);
+});
+
+startScript.on('error', (error) => {
+  console.error(`启动脚本错误: ${error}`);
+  process.exit(1); 
+});
+
+startScript.on('close', (code) => {
+  console.log(`子进程退出，退出码 ${code}`);
+});
+
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
